839f62fc03073e8d89595ad69c894fefabaca67b
kirby-b <kirby.brodie66@gmail.com>
2024-10-20 12:02:39 -0400

    Update Events.gd

--- scripts/Events.gd
+++ scripts/Events.gd
@@ -1,3 +1,10 @@
 extends Node
 
-#Add the events: player death, player win, jumpscare, and player cheated
+# Signals that the player hit the out area
+signal player_fail
+# Signals the player made it to the end of a maze
+signal player_win
+# Signals the player hit a win they shouldnt have(LVL 3)
+signal player_cheat
+# Signals the player hit the "secret end" jumpscare(The way LVL 3 should go)
+signal jumpscare
