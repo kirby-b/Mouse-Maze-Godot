205fe21f613418b4fb746722218cc1df3c19825f
kirby-b <kirby.brodie66@gmail.com>
2024-10-22 20:32:37 -0400

    Create next_level.gd
    For making the end go to next level. LVL 3 will go to an impossible level to troll cheaters, or it will just tell them they cheated. I will decide later

---
+++ scripts/next_level.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+# Gets a .tscn export
+@export_file("*.tscn") var NEXT_LVL: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
