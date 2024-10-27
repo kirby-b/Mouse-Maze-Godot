4a07829ff0fce004e285f3ec7b007060261206d9
kirby-b <kirby.brodie66@gmail.com>
2024-10-27 15:51:36 -0400

    Made the settings menu script

---
+++ scripts/Settings.gd
@@ -0,0 +1,38 @@
+extends PopupMenu
+
+var showing = false
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel") and showing == false:
+		show()
+		showing = true
+	elif Input.is_action_just_pressed("ui_cancel") and showing == true:
+		hide()
+		showing = false
+
+func _on_close_requested():
+	showing = false
+	hide()
+
+func _on_id_pressed(id):
+	if is_item_checkable(id):
+		if is_item_checked(id):
+			set_item_checked(id, false)
+		else:
+			set_item_checked(id, true)
+			
+	if id == 0:
+		if is_item_checked(0):
+			GlobalVars.game_mode = "scare"
+		else:
+			GlobalVars.game_mode = "no-scare"
+	elif id == 1:
+		if is_item_checked(1):
+			GlobalVars.game_mode = "easy"
+		else:
+			GlobalVars.game_mode = "normal"
+	elif id == 3:
+		hide()
+		showing = false
+	elif id == 4:
+		get_tree().quit()
